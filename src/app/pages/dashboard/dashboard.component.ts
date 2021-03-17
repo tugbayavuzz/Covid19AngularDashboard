e60c74f1e701302c93a7b33422f1a12a93e841df
tugbayavuzz <tugbayavuz@yandex.com>
2021-03-17 04:35:11 +0300

    added is date, graphics was displayed, date information was displayed

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,21 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import Chart from 'chart.js';
-import {DataSummary} from '../../models/turkeydata';
-import {DataServicesService} from '../../services/data.service';
-
+import { Component, OnInit } from "@angular/core";
+import Chart from "chart.js";
+import { endOfWeek, isSameWeek, subWeeks } from "date-fns";
+import { DataSummary } from "../../models/turkeydata";
+import { DataServicesService } from "../../services/data.service";
 
 @Component({
-
   // tslint:disable-next-line:component-selector
-    selector: 'dashboard-cmp',
-    moduleId: module.id,
-    templateUrl: 'dashboard.component.html'
+  selector: "dashboard-cmp",
+  moduleId: module.id,
+  templateUrl: "dashboard.component.html",
 })
-
 export class DashboardComponent implements OnInit {
-
   public canvas: any;
   public ctx;
+  public secondCanvas: any;
+  public secondCtx;
+  public thirdCanvas: any;
+  public thirdCtx;
   public chartColor;
   public chartEmail;
   public chartHours;
@@ -31,150 +32,278 @@ export class DashboardComponent implements OnInit {
   weeklyTable = [];
 
   constructor(private dataService: DataServicesService) {}
-   ngOnInit() {
-      this.canvas = document.getElementById('chartEmail');
-      this.ctx = this.canvas.getContext('2d');
-      this.chartEmail = new Chart(this.ctx, {
-        type: 'pie',
-        data: {
-          labels: [1, 2, 3, 4 , 5 , 6 , 7],
-          datasets: [{
-            label: 'Emails',
+  ngOnInit() {
+    this.canvas = document.getElementById("chartEmail");
+    this.ctx = this.canvas.getContext("2d");
+    this.chartEmail = new Chart(this.ctx, {
+      type: "pie",
+      data: {
+        labels: [1, 2, 3, 4, 5, 6, 7],
+        datasets: [
+          {
+            label: "Emails",
             pointRadius: 0,
             pointHoverRadius: 0,
             backgroundColor: [
-              '#4acccd',
-              '#fcc468',
-              '#ef8157',
-              '#6bd098',
-              '#90EE90',
-              '#dcdcdc',
-              '#000000'
+              "#4acccd",
+              "#fcc468",
+              "#ef8157",
+              "#6bd098",
+              "#90EE90",
+              "#dcdcdc",
+              "#000000",
             ],
             borderWidth: 0,
-            data: [this.cases] // son 7 günün verisini dızıye at buraya tanımla.
-          }]
+            data: [this.cases], // son 7 günün verisini dızıye at buraya tanımla.
+          },
+        ],
+      },
+
+      options: {
+        legend: {
+          display: false,
         },
 
-        options: {
+        pieceLabel: {
+          render: "percentage",
+          fontColor: ["white"],
+          precision: 2,
+        },
 
-          legend: {
-            display: false
-          },
+        tooltips: {
+          enabled: false,
+        },
 
-          pieceLabel: {
-            render: 'percentage',
-            fontColor: ['white'],
-            precision: 2
-          },
-
-          tooltips: {
-            enabled: false
-          },
-
-          scales: {
-            yAxes: [{
-
+        scales: {
+          yAxes: [
+            {
               ticks: {
-                display: false
+                display: false,
               },
               gridLines: {
                 drawBorder: false,
-                zeroLineColor: 'transparent',
-                color: 'rgba(255,255,255,0.05)'
-              }
+                zeroLineColor: "transparent",
+                color: "rgba(255,255,255,0.05)",
+              },
+            },
+          ],
 
-            }],
-
-            xAxes: [{
+          xAxes: [
+            {
               barPercentage: 1.6,
               gridLines: {
                 drawBorder: false,
-                color: 'rgba(255,255,255,0.1)',
-                zeroLineColor: 'transparent'
+                color: "rgba(255,255,255,0.1)",
+                zeroLineColor: "transparent",
               },
               ticks: {
                 display: false,
-              }
-            }]
-          },
-        }
-      });
+              },
+            },
+          ],
+        },
+      },
+    });
 
-      const speedCanvas = document.getElementById('speedChart');
+    const speedCanvas = document.getElementById("speedChart");
 
-      const dataFirst = {
-        data: [0, 19, 15, 20, 30, 40, 40, 50, 25, 30, 50, 70],   // Test
-        fill: false,
-        borderColor: '#fbc658',
-        backgroundColor: 'transparent',
-        pointBorderColor: '#fbc658',
-        pointRadius: 4,
-        pointHoverRadius: 4,
-        pointBorderWidth: 8,
-      };
+    const dataFirst = {
+      data: [0, 19, 15, 20, 30, 40, 40, 50, 25, 30, 50, 70], // Test
+      fill: false,
+      borderColor: "#fbc658",
+      backgroundColor: "transparent",
+      pointBorderColor: "#fbc658",
+      pointRadius: 4,
+      pointHoverRadius: 4,
+      pointBorderWidth: 8,
+    };
 
-      const dataSecond = {
-        data: [0, 5, 10, 12, 20, 27, 30, 34, 42, 45, 55, 63],    // vaka
-        fill: false,
-        borderColor: '#51CACF',
-        backgroundColor: 'transparent',
-        pointBorderColor: '#51CACF',
-        pointRadius: 4,
-        pointHoverRadius: 4,
-        pointBorderWidth: 8
-      };
-      const dataThird = {
-        data: [8, 58, 18, 18, 21, 28, 38, 84, 48, 85, 55, 69], // vefat
-        fill: false,
-        borderColor: '#ef8157',
-        backgroundColor: 'transparent',
-        pointBorderColor: '#ef8157',
-        pointRadius: 4,
-        pointHoverRadius: 4,
-        pointBorderWidth: 8
-      };
+    const dataSecond = {
+      data: [0, 5, 10, 12, 20, 27, 30, 34, 42, 45, 55, 63], // vaka
+      fill: false,
+      borderColor: "#51CACF",
+      backgroundColor: "transparent",
+      pointBorderColor: "#51CACF",
+      pointRadius: 4,
+      pointHoverRadius: 4,
+      pointBorderWidth: 8,
+    };
+    const dataThird = {
+      data: [8, 58, 18, 18, 21, 28, 38, 84, 48, 85, 55, 69], // vefat
+      fill: false,
+      borderColor: "#ef8157",
+      backgroundColor: "transparent",
+      pointBorderColor: "#ef8157",
+      pointRadius: 4,
+      pointHoverRadius: 4,
+      pointBorderWidth: 8,
+    };
 
-      const speedData = {
-        labels: ['Pzt', 'Salı', 'Çarş', 'Perş', 'Cuma', 'Cmt', 'Pzr'],
-        datasets: [dataFirst, dataSecond, dataThird]
-      };
+    const speedData = {
+      labels: ["Pzt", "Salı", "Çarş", "Perş", "Cuma", "Cmt", "Pzr"],
+      datasets: [dataFirst, dataSecond, dataThird],
+    };
 
-      const chartOptions = {
+    const chartOptions = {
+      legend: {
+        display: false,
+        position: "top",
+      },
+    };
+
+    const lineChart = new Chart(speedCanvas, {
+      type: "line",
+      hover: false,
+      data: speedData,
+      options: chartOptions,
+    });
+
+    this.dataService.getDailyJsonData().subscribe({
+      next: (res) => {
+        debugger;
+        console.log(res);
+        this.dataSummaries = res;
+        res.forEach((cs) => {
+          if (!Number.isNaN(cs.date)) {
+            this.patients = cs.patients;
+            this.critical = cs.critical;
+            this.cases = cs.cases;
+            this.recovered = cs.recovered;
+            this.pneumoniaPercent = cs.pneumoniaPercent;
+            this.deaths = cs.deaths;
+            this.tests = cs.tests;
+          }
+        });
+        // this.initChart();
+        this.initChartAli();
+      },
+    });
+  }
+
+  initChartAli() {
+    let today = new Date();
+    if (today !== endOfWeek(new Date(), { weekStartsOn: 1 })) {
+      today = subWeeks(today, 1);
+    }
+    // Get this week data
+    let myWeekData = this.dataSummaries.filter((cs) => {
+      let [day, month, year] = cs.date.split("/");
+      let csDate = new Date(+year, +month - 1, +day);
+      let result = isSameWeek(csDate, today, { weekStartsOn: 1 });
+      return result;
+    });
+
+    let patients = myWeekData.map((cs) => cs.patients);
+    let deaths = myWeekData.map((cs) => cs.deaths);
+    let tests = myWeekData.map((cs) => cs.tests);
+
+    console.log("ALI");
+    console.log(patients);
+    console.log(deaths);
+    console.log(tests);
+
+    this.canvas = document.getElementById("chartHours");
+    this.ctx = this.canvas.getContext("2d");
+
+    this.secondCanvas = document.getElementById("chartSecond");
+    this.secondCtx = this.secondCanvas.getContext("2d");
+
+    this.thirdCanvas = document.getElementById("chartThird");
+    this.thirdCtx = this.thirdCanvas.getContext("2d");
+
+    this.drawChart(this.ctx, patients,'patients');
+    this.drawChart(this.secondCtx, deaths, 'deaths');
+    this.drawChart(this.thirdCtx, tests, 'tests');
+  }
+
+  drawChart(ctx, data, type) {
+    let dataSet;
+
+    switch (type) {
+      case "patients":
+        dataSet = {
+          borderColor: "#f17e5d",
+          backgroundColor: "#f17e5d",
+          pointRadius: 0,
+          pointHoverRadius: 0,
+          borderWidth: 3,
+          data: data, // vaka sayısı
+        };
+        break;
+      case "deaths":
+        dataSet = {
+          borderColor: "#fcc468",
+          backgroundColor: "#fcc468",
+          pointRadius: 0,
+          pointHoverRadius: 0,
+          borderWidth: 3,
+          data: data, // ölüm sayısı son 1 hafta
+        };
+        break;
+      case "tests":
+        dataSet = {
+          borderColor: "#6bd098",
+          backgroundColor: "#6bd098",
+          pointRadius: 0,
+          pointHoverRadius: 0,
+          borderWidth: 3,
+          data: data, // test sayısı
+        };
+        break;
+    }
+
+    this.chartHours = new Chart(ctx, {
+      type: "line",
+
+      data: {
+        labels: ["Pzt", "Salı", "Çarş", "Perş", "Cuma", "Cmt", "Pzr"],
+        datasets: [dataSet],
+      },
+      options: {
         legend: {
           display: false,
-          position: 'top'
-        }
-      };
-
-      const lineChart = new Chart(speedCanvas, {
-        type: 'line',
-        hover: false,
-        data: speedData,
-        options: chartOptions
-      });
-
-
-      this.dataService.getDailyJsonData().subscribe({
-        next: (res) => {
-          console.log(res);
-          this.dataSummaries = res;
-          res.forEach((cs) => {
-            if (!Number.isNaN(cs.date)) {
-              this.patients = cs.patients;
-              this.critical = cs.critical;
-              this.cases = cs.cases;
-              this.recovered = cs.recovered;
-              this.pneumoniaPercent = cs.pneumoniaPercent;
-              this.deaths = cs.deaths;
-              this.tests = cs.tests;
-            }
-          });
-          this.initChart();
         },
-      });
 
-    }
+        tooltips: {
+          enabled: false,
+        },
+
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                fontColor: "#9f9f9f",
+                beginAtZero: false,
+                maxTicksLimit: 5,
+                // padding: 20
+              },
+              gridLines: {
+                drawBorder: false,
+                zeroLineColor: "#ccc",
+                color: "rgba(255,255,255,0.05)",
+              },
+            },
+          ],
+
+          xAxes: [
+            {
+              barPercentage: 1.6,
+              gridLines: {
+                drawBorder: false,
+                color: "rgba(255,255,255,0.1)",
+                zeroLineColor: "transparent",
+                display: false,
+              },
+              ticks: {
+                padding: 20,
+                fontColor: "#9f9f9f",
+              },
+            },
+          ],
+        },
+      },
+    });
+  }
 
   initChart() {
     this.datatable = [];
@@ -189,99 +318,97 @@ export class DashboardComponent implements OnInit {
     const totalDeath = [];
     const totalTests = [];
 
-
     for (let i = 0; i < 7; i++) {
       const dailyResult = this.dataSummaries[i];
-      totalPatients.push(dailyResult.patients)
-      totalDeath.push(dailyResult.deaths)
-      totalTests.push(dailyResult.tests)
-       this.weeklyTable[i] = this.datatable[this.datatable.length - 1];
-       this.datatable.length -= 1;
+      totalPatients.push(dailyResult.patients);
+      totalDeath.push(dailyResult.deaths);
+      totalTests.push(dailyResult.tests);
+      this.weeklyTable[i] = this.datatable[this.datatable.length - 1];
+      this.datatable.length -= 1;
     }
 
     console.log(totalPatients);
     console.log(totalDeath);
     console.log(totalTests);
 
-
-    this.canvas = document.getElementById('chartHours');
-    this.ctx = this.canvas.getContext('2d');
+    this.canvas = document.getElementById("chartHours");
+    this.ctx = this.canvas.getContext("2d");
 
     this.chartHours = new Chart(this.ctx, {
-      type: 'line',
+      type: "line",
 
       data: {
-        labels: ['Pzt', 'Salı', 'Çarş', 'Perş', 'Cuma', 'Cmt', 'Pzr'],
-        datasets: [{
-          borderColor: '#6bd098',
-          backgroundColor: '#6bd098',
-          pointRadius: 0,
-          pointHoverRadius: 0,
-          borderWidth: 3,
-          data: totalTests // test sayısı
-        },
+        labels: ["Pzt", "Salı", "Çarş", "Perş", "Cuma", "Cmt", "Pzr"],
+        datasets: [
           {
-            borderColor: '#f17e5d',
-            backgroundColor: '#f17e5d',
+            borderColor: "#6bd098",
+            backgroundColor: "#6bd098",
             pointRadius: 0,
             pointHoverRadius: 0,
             borderWidth: 3,
-            data: totalPatients // vaka sayısı
+            data: totalTests, // test sayısı
           },
           {
-            borderColor: '#fcc468',
-            backgroundColor: '#fcc468',
+            borderColor: "#f17e5d",
+            backgroundColor: "#f17e5d",
             pointRadius: 0,
             pointHoverRadius: 0,
             borderWidth: 3,
-            data: totalDeath // ölüm sayısı son 1 hafta
-          }
-        ]
+            data: totalPatients, // vaka sayısı
+          },
+          {
+            borderColor: "#fcc468",
+            backgroundColor: "#fcc468",
+            pointRadius: 0,
+            pointHoverRadius: 0,
+            borderWidth: 3,
+            data: totalDeath, // ölüm sayısı son 1 hafta
+          },
+        ],
       },
       options: {
         legend: {
-          display: false
+          display: false,
         },
 
         tooltips: {
-          enabled: false
+          enabled: false,
         },
 
         scales: {
-          yAxes: [{
-
-            ticks: {
-              fontColor: '#9f9f9f',
-              beginAtZero: false,
-              maxTicksLimit: 5,
-              // padding: 20
+          yAxes: [
+            {
+              ticks: {
+                fontColor: "#9f9f9f",
+                beginAtZero: false,
+                maxTicksLimit: 5,
+                // padding: 20
+              },
+              gridLines: {
+                drawBorder: false,
+                zeroLineColor: "#ccc",
+                color: "rgba(255,255,255,0.05)",
+              },
             },
-            gridLines: {
-              drawBorder: false,
-              zeroLineColor: '#ccc',
-              color: 'rgba(255,255,255,0.05)'
-            }
+          ],
 
-          }],
-
-          xAxes: [{
-            barPercentage: 1.6,
-            gridLines: {
-              drawBorder: false,
-              color: 'rgba(255,255,255,0.1)',
-              zeroLineColor: 'transparent',
-              display: false,
+          xAxes: [
+            {
+              barPercentage: 1.6,
+              gridLines: {
+                drawBorder: false,
+                color: "rgba(255,255,255,0.1)",
+                zeroLineColor: "transparent",
+                display: false,
+              },
+              ticks: {
+                padding: 20,
+                fontColor: "#9f9f9f",
+              },
             },
-            ticks: {
-              padding: 20,
-              fontColor: '#9f9f9f'
-            }
-          }]
+          ],
         },
-      }
+      },
     });
-
   }
-
-
 }
