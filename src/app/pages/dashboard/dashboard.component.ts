d5aacc4099120041ed1e4aee67506ac4928f569e
tugbayavuzz <tugbayavuz@yandex.com>
2021-03-18 00:20:16 +0300

    graphics was displayed

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -27,145 +27,21 @@ export class DashboardComponent implements OnInit {
   critical = 0;
   pneumoniaPercent = 0;
   tests = 0;
+  date = '';
   dataSummaries: DataSummary[];
   datatable = [];
   weeklyTable = [];
 
   constructor(private dataService: DataServicesService) {}
   ngOnInit() {
-    this.canvas = document.getElementById('chartEmail');
-    this.ctx = this.canvas.getContext('2d');
-    this.chartEmail = new Chart(this.ctx, {
-      type: 'pie',
-      data: {
-        labels: [1, 2, 3, 4, 5, 6, 7],
-        datasets: [
-          {
-            label: 'Emails',
-            pointRadius: 0,
-            pointHoverRadius: 0,
-            backgroundColor: [
-              '#4acccd',
-              '#fcc468',
-              '#ef8157',
-              '#6bd098',
-              '#90EE90',
-              '#dcdcdc',
-              '#000000',
-            ],
-            borderWidth: 0,
-            data: [this.cases], // son 7 günün verisini dızıye at buraya tanımla.
-          },
-        ],
-      },
-
-      options: {
-        legend: {
-          display: false,
-        },
-
-        pieceLabel: {
-          render: 'percentage',
-          fontColor: ['white'],
-          precision: 2,
-        },
-
-        tooltips: {
-          enabled: false,
-        },
-
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                display: false,
-              },
-              gridLines: {
-                drawBorder: false,
-                zeroLineColor: 'transparent',
-                color: 'rgba(255,255,255,0.05)',
-              },
-            },
-          ],
-
-          xAxes: [
-            {
-              barPercentage: 1.6,
-              gridLines: {
-                drawBorder: false,
-                color: 'rgba(255,255,255,0.1)',
-                zeroLineColor: 'transparent',
-              },
-              ticks: {
-                display: false,
-              },
-            },
-          ],
-        },
-      },
-    });
-
-    const speedCanvas = document.getElementById('speedChart');
-
-    const dataFirst = {
-      data: [0, 19, 15, 20, 30, 40, 40, 50, 25, 30, 50, 70], // Test
-      fill: false,
-      borderColor: '#fbc658',
-      backgroundColor: 'transparent',
-      pointBorderColor: '#fbc658',
-      pointRadius: 4,
-      pointHoverRadius: 4,
-      pointBorderWidth: 8,
-    };
-
-    const dataSecond = {
-      data: [0, 5, 10, 12, 20, 27, 30, 34, 42, 45, 55, 63], // vaka
-      fill: false,
-      borderColor: '#51CACF',
-      backgroundColor: 'transparent',
-      pointBorderColor: '#51CACF',
-      pointRadius: 4,
-      pointHoverRadius: 4,
-      pointBorderWidth: 8,
-    };
-    const dataThird = {
-      data: [8, 58, 18, 18, 21, 28, 38, 84, 48, 85, 55, 69], // vefat
-      fill: false,
-      borderColor: '#ef8157',
-      backgroundColor: 'transparent',
-      pointBorderColor: '#ef8157',
-      pointRadius: 4,
-      pointHoverRadius: 4,
-      pointBorderWidth: 8,
-    };
-
-    const speedData = {
-      labels: ['Pzt', 'Salı', 'Çarş', 'Perş', 'Cuma', 'Cmt', 'Pzr'],
-      datasets: [dataFirst, dataSecond, dataThird],
-    };
-
-    const chartOptions = {
-      legend: {
-        display: false,
-        position: 'top',
-      },
-    };
-
-    const lineChart = new Chart(speedCanvas, {
-      type: 'line',
-      hover: false,
-      data: speedData,
-      options: chartOptions,
-    });
-
     this.dataService.getDailyJsonData().subscribe({
       next: (res) => {
-        debugger;
         console.log(res);
         this.dataSummaries = res;
         res.forEach((cs) => {
           if (!Number.isNaN(cs.date)) {
             this.patients = cs.patients;
+            this.date = cs.date;
             this.critical = cs.critical;
             this.cases = cs.cases;
             this.recovered = cs.recovered;
@@ -175,30 +51,30 @@ export class DashboardComponent implements OnInit {
           }
         });
         // this.initChart();
-        this.initChartAli();
+        this.initChart1(), this.initChart2() , this.initChart3();
       },
     });
   }
 
-  initChartAli() {
-    let today = new Date();
-    if (today !== endOfWeek(new Date(), { weekStartsOn: 1 })) {
-      today = subWeeks(today, 1);
+    initChart1() {
+    let today1 = new Date();
+    if (today1 !== endOfWeek(new Date(), { weekStartsOn: 1 })) {
+      today1 = subWeeks(today1, 1);
     }
     // Get this week data
     const myWeekData = this.dataSummaries.filter((cs) => {
       const [day, month, year] = cs.date.split('/');
       const csDate = new Date(+year, +month - 1, +day);
-      const result = isSameWeek(csDate, today, { weekStartsOn: 1 });
+      const result = isSameWeek(csDate, today1, { weekStartsOn: 1 });
       return result;
     });
 
-    const patients = myWeekData.map((cs) => cs.patients);
+    const cases = myWeekData.map((cs) => cs.cases);
     const deaths = myWeekData.map((cs) => cs.deaths);
     const tests = myWeekData.map((cs) => cs.tests);
 
-    console.log('ALI');
-    console.log(patients);
+    console.log('Chart1');
+    console.log(cases);
     console.log(deaths);
     console.log(tests);
 
@@ -211,16 +87,16 @@ export class DashboardComponent implements OnInit {
     this.thirdCanvas = document.getElementById('chartThird');
     this.thirdCtx = this.thirdCanvas.getContext('2d');
 
-    this.drawChart(this.ctx, patients, 'patients');
-    this.drawChart(this.secondCtx, deaths, 'deaths');
-    this.drawChart(this.thirdCtx, tests, 'tests');
+    this.drawChart1(this.ctx, cases, 'cases');
+    this.drawChart1(this.secondCtx, deaths, 'deaths');
+    this.drawChart1(this.thirdCtx, tests, 'tests');
   }
 
-  drawChart(ctx, data, type) {
+  drawChart1(ctx, data, type) {
     let dataSet;
 
     switch (type) {
-      case 'patients':
+      case 'cases':
         dataSet = {
           borderColor: '#f17e5d',
           backgroundColor: '#f17e5d',
@@ -305,7 +181,210 @@ export class DashboardComponent implements OnInit {
     });
   }
 
-  initChart() {
+
+  initChart2() {
+    let today = new Date();
+    if (today !== endOfWeek(new Date(), { weekStartsOn: 1 })) {
+      today = subWeeks(today, 1);
+    }
+    // Get this week data
+    const myWeekData = this.dataSummaries.filter((cs) => {
+      const [day, month, year] = cs.date.split('/');
+      const csDate = new Date(+year, +month - 1, +day);
+      const result = isSameWeek(csDate, today, { weekStartsOn: 1 });
+      return result;
+    });
+
+    const cases = myWeekData.map((cs) => cs.cases);
+
+
+    console.log('Chart2');
+    console.log(cases);
+
+
+    this.canvas = document.getElementById('chartEmail');
+    this.ctx = this.canvas.getContext('2d');
+
+    this.drawChart2(this.ctx, cases, 'cases');
+
+  }
+
+  drawChart2(ctx, data, type) {
+    let dataSet2;
+
+    switch (type) {
+      case 'cases':
+        dataSet2 = {
+          pointRadius: 0,
+          pointHoverRadius: 0,
+          backgroundColor: [
+            '#4acccd',
+            '#fcc468',
+            '#ef8157',
+            '#6bd098',
+            '#90EE90',
+            '#dcdcdc',
+            '#000000',
+          ],
+          borderWidth: 0,
+          data: data,
+        };
+        break;
+    }
+
+    this.chartEmail = new Chart(this.ctx, {
+      type: 'pie',
+
+      data: {
+        labels: [1, 2, 3, 4, 5, 6, 7],
+        datasets: [dataSet2],
+      },
+      options: {
+        legend: {
+          display: false,
+        },
+
+        pieceLabel: {
+          render: 'percentage',
+          fontColor: ['white'],
+          precision: 2,
+        },
+
+        tooltips: {
+          enabled: false,
+        },
+
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                display: false,
+              },
+              gridLines: {
+                drawBorder: false,
+                zeroLineColor: 'transparent',
+                color: 'rgba(255,255,255,0.05)',
+              },
+            },
+          ],
+
+          xAxes: [
+            {
+              barPercentage: 1.6,
+              gridLines: {
+                drawBorder: false,
+                color: 'rgba(255,255,255,0.1)',
+                zeroLineColor: 'transparent',
+              },
+              ticks: {
+                display: false,
+              },
+            },
+          ],
+        },
+      },
+    });
+  }
+
+
+  initChart3() {
+    let today = new Date();
+    if (today !== endOfWeek(new Date(), { weekStartsOn: 1 })) {
+      today = subWeeks(today, 1);
+    }
+    // Get this week data
+    const myWeekData = this.dataSummaries.filter((cs) => {
+      const [day, month, year] = cs.date.split('/');
+      const csDate = new Date(+year, +month - 1, +day);
+      const result = isSameWeek(csDate, today, { weekStartsOn: 1 });
+      return result;
+    });
+
+    const cases = myWeekData.map((cs) => cs.cases);
+    const deaths = myWeekData.map((cs) => cs.deaths);
+    const tests = myWeekData.map((cs) => cs.tests);
+
+
+    console.log('Chart3');
+    console.log(cases);
+    console.log(deaths);
+    console.log(tests);
+
+
+    this.canvas = document.getElementById('speedChart');
+    this.ctx = this.canvas.getContext('2d');
+
+    this.drawChart3(this.ctx, cases, 'cases');
+    this.drawChart3(this.ctx, deaths, 'deaths');
+    this.drawChart3(this.ctx, tests, 'tests');
+
+  }
+
+  drawChart3(ctx, data, type) {
+    let dataFirst, dataSecond, dataThird;
+
+    switch (type) {
+      case 'cases':
+        dataFirst = {
+          data: data, // Test
+          fill: false,
+          borderColor: '#fbc658',
+          backgroundColor: 'transparent',
+          pointBorderColor: '#fbc658',
+          pointRadius: 4,
+          pointHoverRadius: 4,
+          pointBorderWidth: 8,
+        };
+        break;
+
+      case 'tests':
+        dataSecond = {
+          data: data, // vaka
+          fill: false,
+          borderColor: '#51CACF',
+          backgroundColor: 'transparent',
+          pointBorderColor: '#51CACF',
+          pointRadius: 4,
+          pointHoverRadius: 4,
+          pointBorderWidth: 8,
+        };
+        break;
+
+      case 'deaths':
+        dataThird = {
+          data: data, // vefat
+          fill: false,
+          borderColor: '#ef8157',
+          backgroundColor: 'transparent',
+          pointBorderColor: '#ef8157',
+          pointRadius: 4,
+          pointHoverRadius: 4,
+          pointBorderWidth: 8,
+        };
+        break;
+    }
+
+
+    const speedData = {
+      labels: ['Pzt', 'Salı', 'Çarş', 'Perş', 'Cuma', 'Cmt', 'Pzr'],
+      datasets: [dataFirst, dataSecond, dataThird],
+    };
+
+    const chartOptions = {
+      legend: {
+        display: false,
+        position: 'top',
+      },
+    };
+    const canvas = document.getElementById('speedChart');
+    const speedChart = new Chart(canvas, {
+      type: 'line',
+      hover: false,
+      data: speedData,
+      options: chartOptions,
+    });
+  }
+    initChart() {
     this.datatable = [];
     this.weeklyTable = [];
 
@@ -313,21 +392,21 @@ export class DashboardComponent implements OnInit {
       this.datatable.push([cs.date, cs.cases]);
     });
 
-    // son 7 günün :) chartta gösterilmesi ama çalışmıyor .ÇALIŞTI.
-    const totalPatients = [];
+    // son 7 günün  chartta gösterilmesi
+    const totalCases = [];
     const totalDeath = [];
     const totalTests = [];
 
     for (let i = 0; i < 7; i++) {
       const dailyResult = this.dataSummaries[i];
-      totalPatients.push(dailyResult.patients);
+      totalCases.push(dailyResult.cases);
       totalDeath.push(dailyResult.deaths);
       totalTests.push(dailyResult.tests);
       this.weeklyTable[i] = this.datatable[this.datatable.length - 1];
       this.datatable.length -= 1;
     }
 
-    console.log(totalPatients);
+    console.log(totalCases);
     console.log(totalDeath);
     console.log(totalTests);
 
@@ -354,7 +433,7 @@ export class DashboardComponent implements OnInit {
             pointRadius: 0,
             pointHoverRadius: 0,
             borderWidth: 3,
-            data: totalPatients, // vaka sayısı
+            data: totalCases, // vaka sayısı
           },
           {
             borderColor: '#fcc468',
